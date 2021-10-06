079eaf887b082a362ca9d53235491ed99c8bc32b
PaulinaShakirova <pollisha.jeans@gmail.com>
2021-10-06 14:55:49 +0200

    before deploying

--- store/index.js
+++ store/index.js
@@ -11,14 +11,16 @@ export const state = () => ({
 //they are only syncrinous
 export const mutations = {
   updateFoodData: (state, data) => {
-    state.fooddata = data
+    state.fooddata = data;
   }
-}
+};
 //actions can't change the state, but they can commit mutations that can change the state
 //actions are asyncrinous
 export const actions = {
   async getFoodData({ state, commit }) {
-    if (state.fooddata.length) {return}
+    if (state.fooddata.length) {
+      return;
+    }
     try {
       await fetch(
         "https:dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants",
@@ -30,6 +32,7 @@ export const actions = {
         }
       )
         .then(response => response.json())
+
         .then(data => {
           commit("updateFoodData", data);
         });
