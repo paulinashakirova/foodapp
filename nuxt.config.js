a3132b1aaea3376466fd3da3365c82858640d939
PaulinaShakirova <pollisha.jeans@gmail.com>
2021-10-11 22:29:27 +0200

    deployed food app on Netlify
    Vuelidate, Vuex, plugins, router, fetch

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     `~/plugins/getfood.server.js`,
-    `~/plugins/vuelidate.js`
+    `~/plugins/vuelidate.js`,
+    `~/plugins/composition-api.js`
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
